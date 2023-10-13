b328a87fc4c03f4aa42e8cae0c655391456406d8
sagarcial <santiago.alexander270905@gmail.com>
2023-10-13 07:49:24 -0500

    version 0.8

--- archivo.py
+++ archivo.py
@@ -6,13 +6,33 @@ class LaberintoManager:
         self.laberinto = None
 
     def cargar_laberinto_desde_archivo(self, archivo):
-        # Lógica para cargar el laberinto desde un archivo
-        pass
+        """Lógica para cargar el laberinto desde un archivo."""
+        with open(archivo, "r") as f:
+            lineas = f.readlines()
+
+        # Crear el laberinto
+        self.laberinto = Laberinto()
+
+        # Agregar las paredes
+        for linea in lineas:
+            for caracter in linea:
+                if caracter == "1":
+                    self.laberinto.agregar_elemento(Pared(caracter))
+                elif caracter == "0":
+                    self.laberinto.agregar_elemento(EspacioLibre(caracter))
+                elif caracter == "X":
+                    self.laberinto.agregar_elemento(Jugador(caracter))
+                elif caracter == "W":
+                    self.laberinto.agregar_elemento(Meta(caracter))
+
+        # Establecer el tamaño del laberinto
+        self.laberinto.establecer_tamano(len(lineas), len(lineas[0]))
 
     def dibujar_laberinto(self):
-        # Lógica para dibujar el laberinto
-        pass
+        """Lógica para dibujar el laberinto."""
+        for fila in self.laberinto.obtener_filas():
+            for elemento in fila:
+                print(elemento.obtener_caracter(), end="")
+            print()
 
-    def mover_jugador(self, direccion):
-        # Lógica para mover el jugador
-        pass
+  
