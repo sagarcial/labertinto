15a5acd042c9a5b079cb65496a2abd1b136448b4
Santiago Alexander Garcia Liscano <83739733+sagarcial@users.noreply.github.com>
2023-10-13 07:40:49 -0500

    Update main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+from laberinto_manager import LaberintoManager
+
+def main():
+    laberinto_manager = LaberintoManager()
+    laberinto_manager.cargar_laberinto_desde_archivo("laberinto.txt")
+    # Inicializar Pygame y bucle principal
+    while True:
+        # Manejar eventos de Pygame
+        # Dibujar el laberinto
+        # Mover al jugador
+        # Otros aspectos del juego
+
+if __name__ == "__main__":
+    main()
