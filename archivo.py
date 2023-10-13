800c4374707d7203addafd35a96a92a53067e36b
Santiago Alexander Garcia Liscano <83739733+sagarcial@users.noreply.github.com>
2023-10-13 07:42:52 -0500

    Update archivo.py

---
+++ archivo.py
@@ -0,0 +1,18 @@
+from laberinto import Laberinto
+from elemento_laberinto import Pared, Jugador, EspacioLibre, Meta
+
+class LaberintoManager:
+    def __init__(self):
+        self.laberinto = None
+
+    def cargar_laberinto_desde_archivo(self, archivo):
+        # Lógica para cargar el laberinto desde un archivo
+        pass
+
+    def dibujar_laberinto(self):
+        # Lógica para dibujar el laberinto
+        pass
+
+    def mover_jugador(self, direccion):
+        # Lógica para mover el jugador
+        pass
